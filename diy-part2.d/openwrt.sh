874d6eb6e76c6d4d3ac2f5f11fdaba0f2c03b9b2
liubei66 <1967780821@qq.com>
2026-08-26 16:55:17 +0800

    fix nfs init script modify

--- diy-part2.d/openwrt.sh
+++ diy-part2.d/openwrt.sh
@@ -20,7 +20,7 @@ sed -i '/CONFIG_PACKAGE_mihomo-alpha/d' .config
 
 [ -f package/feeds/luci/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json ] && sed -i 's#admin/services/ksmbd#admin/nas/ksmbd#g' ./package/feeds/luci/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
 
-[ -f package/feeds/luci/feeds/luci/applications/luci-app-nfs/root/etc/init.d/nfs ] && cp "$GITHUB_WORKSPACE/diy-part2.d/.diy/nfs" package/feeds/luci/feeds/luci/applications/luci-app-nfs/root/etc/init.d/nfs
+[ -f package/feeds/luci/luci-app-nfs/root/etc/init.d/nfs ] && cp "$GITHUB_WORKSPACE/diy-part2.d/.diy/nfs" package/feeds/luci/luci-app-nfs/root/etc/init.d/nfs
 
 [ -d package/base-files/files/etc/profile.d ] && cp "$GITHUB_WORKSPACE/diy-part2.d/.diy/30-sysinfo.sh" package/base-files/files/etc/profile.d/
 
