4358790e984ac72525db71709b1f217da74b852c
liubei66 <1967780821@qq.com>
2026-08-26 13:30:05 +0800

    add custom modify in base-files

--- diy-part2.d/immortalwrt.sh
+++ diy-part2.d/immortalwrt.sh
@@ -33,6 +33,10 @@ sed -i '/CONFIG_PACKAGE_luci-i18n-clientstatus-zh-cn/d' .config
 sed -i '/CONFIG_PACKAGE_luci-app-clientstatus/d' .config
 sed -i '/CONFIG_PACKAGE_mihomo-alpha/d' .config
 
-sed -i 's#admin/services/ksmbd#admin/nas/ksmbd#g' ./package/feeds/luci/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
+[ -f package/feeds/luci/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json ] && sed -i 's#admin/services/ksmbd#admin/nas/ksmbd#g' ./package/feeds/luci/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
 
+[ -f package/feeds/luci/feeds/luci/applications/luci-app-nfs/root/etc/init.d/nfs ] && cp "$GITHUB_WORKSPACE/diy-part2.d/.diy/nfs" package/feeds/luci/feeds/luci/applications/luci-app-nfs/root/etc/init.d/nfs
 
+[ -d package/base-files/files/etc/profile.d ] && cp "$GITHUB_WORKSPACE/diy-part2.d/.diy/30-sysinfo.sh" package/base-files/files/etc/profile.d/
+
+[ -d package/base-files/files/etc ] && cp "$GITHUB_WORKSPACE/diy-part2.d/.diy/banner" package/base-files/files/etc/banner
