f6b3a1c873f5e62e3af6a6c95f0df793220b9292
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-08-31 11:16:38 +0800

    Update immortalwrt.sh

--- diy-part2.d/immortalwrt.sh
+++ diy-part2.d/immortalwrt.sh
@@ -34,7 +34,7 @@ sed -i '/CONFIG_PACKAGE_luci-app-clientstatus/d' .config
 sed -i '/CONFIG_PACKAGE_mihomo-alpha/d' .config
 
 #enable fuse
-[ "$ARCHITECTURE" = "x86_64" ] && echo 'enable fuse for x86 fireware' && echo 'CONFIG_FUSE_FS=y' >> target/linux/x86/64/config-6.18
+[ "$ARCHITECTURE" = "x86_64" ] && echo 'enable fuse for x86 firmware' && echo 'CONFIG_FUSE_FS=y' >> target/linux/x86/64/config-6.18
 
 [ -f package/feeds/luci/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json ] && sed -i 's#admin/services/ksmbd#admin/nas/ksmbd#g' ./package/feeds/luci/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
 
